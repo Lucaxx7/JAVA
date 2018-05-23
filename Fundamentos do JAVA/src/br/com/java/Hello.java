a8485308de8b2ddaa8a8c0b5abf1d03b2bc6b625
luczz134525 <38111159+luczz134525@users.noreply.github.com>
2018-05-23 16:11:10 -0300

    Add files via upload

---
+++ Fundamentos do JAVA/src/br/com/java/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.java;
+
+/**
+ * @author lucas.soliveira37
+ *
+ */
+public class Hello {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Lucas");
+	}
+
+}
